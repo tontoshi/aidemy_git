3c05b52f9223b74163d49d5604168e822c979d00
Toshikazu Sugimoto <todhisugi@gmail.com>
2022-10-21 20:01:52 +0900

    add python program

---
+++ show.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import random
+
+def main():
+
+    with open("./hyakunin.txt", encoding="utf-8") as f:
+        wakas = [s.strip() for s in f.readlines()]
+        print("今日の一句" + wakas[random.randrange(len(wakas))])
+
+if __name__=="__main__":
+    main()
+
+"""
+	pass
+"""
